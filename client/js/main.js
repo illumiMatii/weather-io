efa52d9be9beb8fd09ae257f805a73041443c5e0
illumiMatii <illumimatii@github.com>
2020-05-08 21:02:43 +0200

    app show temp, pressure, humidity of given city from input

--- client/js/main.js
+++ client/js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     input.addEventListener("keypress", (e) => {
         if(e.key === "Enter"){
             getValueOfInput();
+            input.value = "";
         }
     });
 });
@@ -11,18 +12,28 @@ const input = document.querySelector("#search");
 const cityCountry = document.querySelector(".city-country");
 const weatherInfo = document.querySelector(".info");
 
-let tag = document.querySelector(".city-country > h2");
+
+let tag = document.querySelector(".city-country > h1");
 let obj;
+let temp = document.createElement("h2");
+let pressure = document.createElement("h2");
+let humidity = document.createElement("h2");
+
 
 function getValueOfInput() {
     let value = input.value.trim();
 
     if(value) {
         cityCountry.style.display = "flex";
+        weatherInfo.style.display = "block";
+        weatherInfo.appendChild(temp);
+        weatherInfo.appendChild(pressure);
+        weatherInfo.appendChild(humidity);
         setWeather(value);
 
     } else {
         cityCountry.style.display = "flex";
+        weatherInfo.style.display = "none";
         tag.textContent = "Sorry, something went wrong provide City name again";
     }
 }
@@ -33,9 +44,13 @@ function getValueOfInput() {
             .then(data => {obj = data})
             .then(() => {
                 tag.textContent = `${city}, ${obj.sys.country}`;
+                temp.textContent = `Temperature: ${Math.round(obj.main.temp)}°C`;
+                pressure.textContent = `Pressure: ${obj.main.pressure} hPa`;
+                humidity.textContent = `Humidity: ${obj.main.humidity}`;
             })
             .catch(() => {
                 cityCountry.style.display = "flex";
+                weatherInfo.style.display = "none";
                 tag.textContent = "Sorry, something went wrong provide City name again";
             });
 }
